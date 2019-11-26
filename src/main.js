19454a4563ddce644e7c8949117a202b817d0329
Bhavyam Goyal <bhavyam@outscal.com>
2019-11-26 21:52:41 +0530

    Space ship shooting on space press and ship moving using left right arrow keys.

---
+++ src/main.js
@@ -0,0 +1,123 @@
+var physicsConfig = {
+    default: 'arcade',
+    arcade: {
+        debug: true
+    }
+}
+
+var phaserConfig={
+	type: Phaser.AUTO,
+	width: 800,
+	hieght: 600,
+	physics: physicsConfig,
+	scene: {
+		preload: preload,
+		create: create,
+		update: update,
+
+		extend: {
+			makePlayer: makePlayer
+		}
+	}
+};
+
+var player;
+var fireButton;
+var leftKey;
+var rightKey;
+var spacebg;
+var bullets,bullet;
+var game = new Phaser.Game(phaserConfig);
+var bulletTime=0;
+function preload(){
+	//console.log("calling Preload");
+	this.load.image('player','assets/player.png');
+	this.load.image('spaceBG','assets/spaceBG.jpg');
+	this.load.image('bullet','assets/bullet.png');
+}
+	
+function create(){
+	 var Bull = new Phaser.Class({
+
+			Extends: Phaser.GameObjects.Image,
+			initialize:function Bullet (scene)
+			{
+				Phaser.GameObjects.Image.call(this, scene, player.x, player.y-80, 'bullet');
+				this.debugShowBody=false;
+				this.scale=0.07;
+				this.debugShowVelocity=false;
+				
+			},
+			fire: function (x,y)
+			{
+			 this.setPosition(x, y - 50);
+				this.setActive(true);
+				this.setVisible(true);
+			},
+			update: function (time, delta)
+			{
+				this.y -= 15;// * delta;
+				//console.log(this.y);
+				if (this.y < 00)
+				{
+					this.setActive(false);
+					this.setVisible(false);
+				}
+			}
+
+		});
+	//console.log("calling Create");
+	spacebg=this.add.tileSprite(0,0,this.sys.canvas.width*2,this.sys.canvas.height*2,'spaceBG');
+	player=this.makePlayer(this.sys.canvas.width/2,this.sys.canvas.height-10);
+	player.setScale(.05);
+	//this.physics.enable(player,Phaser.Physics.ARCADE);
+	//console.log(spacebg._tilePosition);
+	bv=-0.7;
+	fireButton=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+	leftKey=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+	rightKey=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+	//bullets=this.physics.add.group({maxSize:5});
+	//bullets.enableBody=true;
+	//console.log(bullets);
+	 bullets = this.add.group({
+        classType: Bull,
+        maxSize: 10,
+        runChildUpdate: true
+    });
+	bullets.setOrigin(0.5,1);
+
+	
+}
+	
+function update(){   
+	spacebg._tilePosition.y +=bv;
+	if(rightKey.isDown && player.x < this.sys.canvas.width - player.displayWidth* player.originX){
+		player.x+=player.props.speed;
+	}else if(leftKey.isDown && player.x >0+player.displayWidth*player.originX){
+		player.x-=player.props.speed;
+	}
+	if(fireButton.isDown){
+		if(this.time.now>bulletTime){
+			bullet=bullets.get();
+			if(bullet){
+			bullet.fire(player.x,player.y);
+			}//bullet=bullets.getFirst(false,true, player.x, player.y-80,'bullet',0, false);
+			//bullet.scale=0.071;
+			
+			//bullet.visible=true;
+			
+		
+			//console.log(bullet);
+
+			bulletTime=this.time.now+150;
+		}
+	}
+}
+
+function makePlayer(x,y){
+	//console.log("making player"+ x+" "+y);
+	var player = this.add.image(x,y,"player").setOrigin(0.5,1);
+	player.props={};
+	player.props.speed=10;
+	return player;
+}
